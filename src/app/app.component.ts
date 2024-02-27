e291d432db53775ebac114f02a91a118d70aef83
SaadS19 <121642559+SaadS19@users.noreply.github.com>
2024-02-27 17:16:32 +0500

    Modal View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  //Pass Data from Parent to Child
-  //title = 'Pass Data from Parent to child';
-  // data: number = 10;
-  // updateData() {
-  //   this.data = Math.floor(Math.random() * 10);
-  // }
-  //
-  // title = 'Pass Data from Child to Parent';
-  // name = '';
-  // updateData(value: string) {
-  //   this.name = value;
-  // }
   data: any = [
     {
       name: 'Muarij Fasih',
